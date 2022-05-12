c8db0ac7b7b1a0c8612d7031dbca665cbe640855
Daffa <daffa.ahmadrivaldi.tik19@mhsw.pnj.ac.id>
2022-05-12 20:56:20 +0700

    first commit

---
+++ src/Components/Body/Cart/CartItem.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+
+import {
+  changeQuantity,
+  removeItem,
+} from "../../../Redux/actionCreators/cartActions";
+import { SubTotalCalculator } from "../../Helper/Helper";
+import "./Cart.css";
+
+const CartItem = (props) => {
+  const { id, title, image, price, quantity } = props.item;
+  const [value, setValue] = useState(quantity);
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
+    if (e.target.value > 0 && e.target.value <= 20) {
+      let content = { id, value: e.target.value };
+      dispatch(changeQuantity(content));
+    }else{
+      alert("Max quantity adalah 20 & quantity tidak boleh kosong")
+    }
+  };
+  const handleRemoveItem = () => {
+    dispatch(removeItem(id));
+  };
+  useEffect(() => {
+    setValue(quantity);
+  }, [quantity]);
+  const subTotal = SubTotalCalculator(price, quantity);
+  return (
+    <div className="cart-container">
+      <img className="cart-image" src={image} alt="" />
+      <div className="close-box">
+        <button className="cart-close-btn" onClick={handleRemoveItem}>
+          X
+        </button>
+      </div>
+      <h6 className="cart-title">{title}</h6>
+      <div className="cart-content">
+        <p> $ {price} X </p>
+        <input
+          type="number"
+          min="1"
+          name="quantity"
+          onChange={handleChange}
+          value={value}
+          id="quantity"
+        />
+        <p> &nbsp; = $ {subTotal}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
